30141799815e06b218fc863c1968c2fc49c06a61
msi <lg.mobile0808@gmail.com>
2025-12-28 17:50:57 +0200

    fix

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 
-# Exit the script immediately if any command exits with a non-zero status
 set -e
 
-# Function to handle errors with custom messages
 handle_error() {
     echo "Error: $1"
     exit 1
 }
 
-# Navigate to the application directory
-PROJECT_DIR="${PROJECT_DIR:-$HOME/src/py-fastapi-homework-5-ec2-deploy-task}"
+PROJECT_DIR="/home/ubuntu/src/py-fastapi-homework-5-task"
 
-cd "$PROJECT_DIR" || { echo "Error: Failed to navigate to the application directory."; exit 1; }
-# Fetch the latest changes from the remote repository
-echo "Fetching the latest changes from the remote repository..."
-git fetch origin main || handle_error "Failed to fetch updates from the 'origin' remote."
+cd "$PROJECT_DIR" || handle_error "Failed to navigate to $PROJECT_DIR"
 
-# Reset the local repository to match the remote 'main' branch
-echo "Resetting the local repository to match 'origin/main'..."
-git reset --hard origin/main || handle_error "Failed to reset the local repository to 'origin/main'."
+echo "Fetching changes..."
+git fetch origin main || handle_error "Git fetch failed"
 
-# (Optional) Pull any new tags from the remote repository
-echo "Fetching tags from the remote repository..."
-git fetch origin --tags || handle_error "Failed to fetch tags from the 'origin' remote."
+echo "Resetting repository..."
+git reset --hard origin/main || handle_error "Git reset failed"
 
-# Build and run Docker containers with Docker Compose v2
-docker compose -f docker-compose-prod.yml up -d --build || handle_error "Failed to build and run Docker containers using docker-compose-prod.yml."
+echo "Updating tags..."
+git fetch origin --tags || handle_error "Git fetch tags failed"
 
-# Print a success message upon successful deployment
+echo "Restarting containers..."
+docker compose -f docker-compose-prod.yml up -d --build || handle_error "Docker compose failed"
+
 echo "Deployment completed successfully."
