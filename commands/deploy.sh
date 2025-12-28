f15eb565077fbdaca44d6af20c659356aadd9e8d
msi <lg.mobile0808@gmail.com>
2025-12-28 18:25:23 +0200

    fix

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -9,21 +9,18 @@ handle_error() {
 
 PROJECT_DIR="/home/ubuntu/src/py-fastapi-homework-5-ec2-deploy-task"
 
-cd "$PROJECT_DIR" || handle_error "Failed to navigate to the application directory."
+cd "$PROJECT_DIR" || handle_error "Failed to navigate to $PROJECT_DIR"
 
-echo "Fetching the latest changes..."
-git fetch origin main || handle_error "Failed to fetch updates."
+echo "Fetching changes..."
+git fetch origin main || handle_error "Git fetch failed"
 
-echo "Resetting to origin/main..."
-git reset --hard origin/main || handle_error "Failed to reset."
+echo "Resetting repository..."
+git reset --hard origin/main || handle_error "Git reset failed"
 
-echo "Fetching tags..."
-git fetch origin --tags || handle_error "Failed to fetch tags."
-
-echo "Stopping old containers to avoid conflicts..."
-docker compose -f docker-compose-prod.yml down || echo "No containers to stop."
+echo "Stopping old containers..."
+docker compose -f docker-compose-prod.yml -p theater down || echo "No containers to stop."
 
 echo "Building and running containers..."
-docker compose -f docker-compose-prod.yml up -d --build || handle_error "Failed to build containers."
+docker compose -f docker-compose-prod.yml -p theater up -d --build || handle_error "Docker compose failed"
 
 echo "Deployment completed successfully."
