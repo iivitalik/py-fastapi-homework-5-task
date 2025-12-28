cca8f8049bcb2242eb37c22883ed4ea4b144a52e
msi <lg.mobile0808@gmail.com>
2025-12-28 18:20:47 +0200

    fix

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -7,19 +7,20 @@ handle_error() {
     exit 1
 }
 
-PROJECT_DIR="/home/ubuntu/src/py-fastapi-homework-5-task"
+PROJECT_DIR="/home/ubuntu/src/py-fastapi-homework-5-ec2-deploy-task"
 
-cd "$PROJECT_DIR" || handle_error "Failed to navigate to the application directory."
+cd "$PROJECT_DIR" || handle_error "Failed to navigate to $PROJECT_DIR"
 
-echo "Fetching the latest changes from the remote repository..."
-git fetch origin main || handle_error "Failed to fetch updates from the 'origin' remote."
+echo "Fetching changes..."
+git fetch origin main || handle_error "Git fetch failed"
 
-echo "Resetting the local repository to match 'origin/main'..."
-git reset --hard origin/main || handle_error "Failed to reset the local repository to 'origin/main'."
+echo "Resetting repository..."
+git reset --hard origin/main || handle_error "Git reset failed"
 
-echo "Fetching tags from the remote repository..."
-git fetch origin --tags || handle_error "Failed to fetch tags from the 'origin' remote."
+echo "Updating tags..."
+git fetch origin --tags || handle_error "Git fetch tags failed"
 
-docker compose -f docker-compose-prod.yml up -d --build || handle_error "Failed to build and run Docker containers."
+echo "Restarting containers..."
+docker compose -f docker-compose-prod.yml up -d --build || handle_error "Docker compose failed"
 
 echo "Deployment completed successfully."
